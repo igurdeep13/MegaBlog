9275533957246caa25049795ca435a2eab631a3a
Gurdeep Singh <g.s.basantpuria@gmail.com>
2024-11-12 23:58:03 -0500

    Created new file config.js

---
+++ src/appwrite/config.js
@@ -0,0 +1,7 @@
+import conf from "../conf.js";
+import { Client, ID, Databases, Storage, Query } from "appwrite";
+
+export class Service {}
+
+const service = new Service();
+export default service;
