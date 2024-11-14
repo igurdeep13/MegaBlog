f7f729a82650a8a01eb9825990a0f9c15775985a
Gurdeep Singh <g.s.basantpuria@gmail.com>
2024-11-14 00:45:42 -0500

    create create, update and delete document method

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -1,7 +1,68 @@
 import conf from "../conf.js";
 import { Client, ID, Databases, Storage, Query } from "appwrite";
 
-export class Service {}
+export class Service {
+  client = new Client();
+  databases;
+  bucket;
+
+  constructor() {
+    this.client
+      .setEndpoint(conf.appwriteUrl)
+      .setProject(conf.appwriteProjectId);
+    this.databases = new Account(this.client);
+    this.bucket = new Storage(this.client);
+  }
+
+  async createPost({ title, slug, content, featuredImages, status, userId }) {
+    try {
+      return await this.databases.createDocument(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        slug,
+        {
+          title,
+          content,
+          featuredImages,
+          status,
+        }
+      );
+    } catch (error) {
+      console.log("Appwrite service :: createPost :: error", error);
+    }
+  }
+  async updatePost(slug, { title, content, featuredImages, status }) {
+    try {
+      return await this.databases.createDocument(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        slug,
+        {
+          title,
+          content,
+          featuredImages,
+          status,
+        }
+      );
+    } catch (error) {
+      console.log("Appwrite service :: createPost :: error", error);
+    }
+  }
+
+  async deletePost(slug) {
+    try {
+      return await this.databases.deleteDocument(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        slug
+      );
+      return true;
+    } catch (error) {
+      console.log("Appwrite service :: createPost :: error", error);
+      return false;
+    }
+  }
+}
 
 const service = new Service();
 export default service;
