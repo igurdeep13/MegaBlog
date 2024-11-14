3e64920758981431c34847b7e7c26e55c3ec49e2
Gurdeep Singh <g.s.basantpuria@gmail.com>
2024-11-14 15:45:53 -0500

    Add components(header, footer)

---
+++ src/store/authSlice.js
@@ -0,0 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialSlice = {
+  status: false,
+  userData: null,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    login: (state, action) => {
+      state.status = true;
+      state.userData = action.payload;
+    },
+    logout: (state) => {
+      state.status = false;
+      state.userData = null;
+    },
+  },
+});
+
+// Importing individual reducers
+export const { login, logout } = authSlice.actions;
+
+// Importing authSlice.reducer
+export default authSlice.reducer;
