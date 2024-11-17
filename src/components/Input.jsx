83ff571b9f3cea72c7284ef2ae3b1b41b02ffa2a
Gurdeep Singh <g.s.basantpuria@gmail.com>
2024-11-16 21:25:38 -0500

    more component add

---
+++ src/components/Input.jsx
@@ -0,0 +1,25 @@
+import React, { useId } from "react";
+
+const Input = React.forwardRef(function Input(
+  { label, type = "text", className = "", ...props },
+  ref
+) {
+  return (
+    <div className="w-full">
+      {label && (
+        <label className="inline-block mb-1 pl-1" htmlFor={props.id}>
+          {label}
+        </label>
+      )}
+      <input
+        type={type}
+        className={`${className}`}
+        ref={ref}
+        {...props}
+        id={id}
+      />
+    </div>
+  );
+});
+
+export default Input;
